01aa02786ed860a6104fa88401a98521a05ef181
Yaakov Azat <azat@Azats-Mac.local>
2020-04-21 05:03:43 -0700

    make the program great again

--- setup.py
+++ setup.py
@@ -1,51 +1,83 @@
-
 #!/usr/bin/env python3
-__author__ = "Azat Artificial Intelligence, LLP (AzatAI)"
-__copyright__ = "Copyright 2020, AzatAI"
-__credits__ = ["Yaakov Azat", ]
-__license__ = "MIT"
-__version__ = "0.0.0"
-__maintainer__ = "Yaakov Azat"
-__email__ = "yaakovazat@gmail.com"
-__status__ = "Development"
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
 
-"""
+import os
+import re
+import sys
 
-                                                                                                                                    
-                                                                                                                                    
-               AAA                                                          tttt                        AAA               IIIIIIIIII
-              A:::A                                                      ttt:::t                       A:::A              I::::::::I
-             A:::::A                                                     t:::::t                      A:::::A             I::::::::I
-            A:::::::A                                                    t:::::t                     A:::::::A            II::::::II
-           A:::::::::A           zzzzzzzzzzzzzzzzz  aaaaaaaaaaaaa  ttttttt:::::ttttttt              A:::::::::A             I::::I  
-          A:::::A:::::A          z:::::::::::::::z  a::::::::::::a t:::::::::::::::::t             A:::::A:::::A            I::::I  
-         A:::::A A:::::A         z::::::::::::::z   aaaaaaaaa:::::at:::::::::::::::::t            A:::::A A:::::A           I::::I  
-        A:::::A   A:::::A        zzzzzzzz::::::z             a::::atttttt:::::::tttttt           A:::::A   A:::::A          I::::I  
-       A:::::A     A:::::A             z::::::z       aaaaaaa:::::a      t:::::t                A:::::A     A:::::A         I::::I  
-      A:::::AAAAAAAAA:::::A           z::::::z      aa::::::::::::a      t:::::t               A:::::AAAAAAAAA:::::A        I::::I  
-     A:::::::::::::::::::::A         z::::::z      a::::aaaa::::::a      t:::::t              A:::::::::::::::::::::A       I::::I  
-    A:::::AAAAAAAAAAAAA:::::A       z::::::z      a::::a    a:::::a      t:::::t    tttttt   A:::::AAAAAAAAAAAAA:::::A      I::::I  
-   A:::::A             A:::::A     z::::::zzzzzzzza::::a    a:::::a      t::::::tttt:::::t  A:::::A             A:::::A   II::::::II
-  A:::::A               A:::::A   z::::::::::::::za:::::aaaa::::::a      tt::::::::::::::t A:::::A               A:::::A  I::::::::I
- A:::::A                 A:::::A z:::::::::::::::z a::::::::::aa:::a       tt:::::::::::ttA:::::A                 A:::::A I::::::::I
-AAAAAAA                   AAAAAAAzzzzzzzzzzzzzzzzz  aaaaaaaaaa  aaaa         ttttttttttt AAAAAAA                   AAAAAAAIIIIIIIIII
-                                                                                                                                    
-                                                                                                                                    
-                                                                                                                                    
-                                                                                                                                    
-                                                                                                                                                                                                          
-"""
-from setuptools import setup
+from codecs import open
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist')
+    os.system('twine upload dist/*')
+    os.system('rm -rf dist')
+    os.system('rm -rf *.egg-info')
+    sys.exit()
+
+
+requires = [
+    # Add prerequired packages here.
+    'click'
+]
+
+
+about = {}  # create a empty dictionary to store the content of the __version__.py file
+# execute the __version__.py file and get the content, save to the dictionary about.
+with open(os.path.join(here, 'gitset', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
-    name='gitset',
-    version='0.1',
-    py_modules=['gitset'],
-    install_requires=[
-        'Click',
-    ],
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    # package_dir={'startpkg': 'src'},
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=requires,
     entry_points='''
         [console_scripts]
-        gitset=gitset:cli
+        gitset=gitset.cli:cli
     ''',
+    license=about['__license__'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    project_urls={
+        'Documentation': 'https://azat.ai',
+        'Source': 'https://github.com/AzatAI/gitset',
+    },
 )
